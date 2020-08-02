b2181c7fc061678a4f3ecba800b19bbc75e18df3
mtdr <paul.osmolovsky@gmail.com>
2020-08-02 22:41:46 +0300

    Поддержка Swagger'a

--- src/main/java/com/edu/mtdr/soapfacade/JsonController.java
+++ src/main/java/com/edu/mtdr/soapfacade/JsonController.java
@@ -3,12 +3,14 @@ package com.edu.mtdr.soapfacade;
 import com.edu.mtdr.soapfacade.jsonMsgs.MultiplyJsonReq;
 import com.edu.mtdr.soapfacade.jsonMsgs.MultiplyJsonResponse;
 import com.edu.mtdr.soapfacade.wsdl.MultiplyResponse;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value="Калькулятор")
 public class JsonController {
     private final SoapClient soapClient;
 
