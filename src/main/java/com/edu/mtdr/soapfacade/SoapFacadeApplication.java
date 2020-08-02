1ec0e73ab54fb69b4d1650923a716230b8086a1e
mtdr <paul.osmolovsky@gmail.com>
2020-08-02 23:07:05 +0300

    Убран тестовый бин

--- src/main/java/com/edu/mtdr/soapfacade/SoapFacadeApplication.java
+++ src/main/java/com/edu/mtdr/soapfacade/SoapFacadeApplication.java
@@ -1,25 +1,11 @@
 package com.edu.mtdr.soapfacade;
 
-import com.edu.mtdr.soapfacade.service.SoapClient;
-import com.edu.mtdr.soapfacade.wsdl.MultiplyResponse;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SoapFacadeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SoapFacadeApplication.class, args);
     }
-
-    @Bean
-    CommandLineRunner lookup(SoapClient soapClient) {
-        return args -> {
-            MultiplyResponse response = soapClient.getMultiplyResult(5, 10);
-            System.err.println(response.getMultiplyResult());
-        };
-    }
-
 }
